27f637cdd322338ef82d928ae43c803b6d9f5bf8
Radek Semanský <rado.semansky@gmail.com>
2021-08-03 00:15:49 +0200

    ,,

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ const char *soft_ap_password = "sedykocur";
 //const char *password = "121213765";
 char NazovSiete[30];
 char Heslo[30];
-
+ 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
